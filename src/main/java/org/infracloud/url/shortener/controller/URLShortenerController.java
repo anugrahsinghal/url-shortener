1772c596fdd0572f1db47ad919153ac03d29c4d3
anugrahsinghal <anugrahsinghal1@gmail.com>
2021-08-14 20:45:37 +0000

    added some swagger docs

--- src/main/java/org/infracloud/url/shortener/controller/URLShortenerController.java
+++ src/main/java/org/infracloud/url/shortener/controller/URLShortenerController.java
@@ -1,7 +1,9 @@
 package org.infracloud.url.shortener.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.infracloud.url.shortener.dto.ShortenUrlRequest;
 import org.infracloud.url.shortener.excpetions.InvalidExpirationException;
 import org.infracloud.url.shortener.excpetions.ShortLinkExpiredException;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+@Log4j2
 @Validated
 @RequiredArgsConstructor
 @RestController
@@ -24,13 +27,17 @@ public class URLShortenerController {
 
 	private final URLShortener urlShortenerService;
 
+	@Operation(description = "Supply Request with Valid URL to generate a short URL")
 	@PostMapping("/shorten")
 	public String shortenURL(@RequestBody @Valid ShortenUrlRequest request) throws InvalidExpirationException {
+		log.info("request [{}]", request);
 		return urlShortenerService.shorten(request);
 	}
 
+	@Operation(description = "Supply valid short URL and you are re-directed to the original long url")
 	@GetMapping(value = "{shortLink}")
 	public RedirectView resolveAndRedirect(@PathVariable String shortLink) throws ShortLinkNotFoundException, ShortLinkExpiredException {
+		log.info("shortLink [{}]", shortLink);
 		String originalURL = urlShortenerService.resolveOriginalURL(shortLink);
 
 		return new RedirectView(originalURL);
--- src/main/java/org/infracloud/url/shortener/dto/ShortenUrlRequest.java
+++ src/main/java/org/infracloud/url/shortener/dto/ShortenUrlRequest.java
@@ -19,7 +19,9 @@ public class ShortenUrlRequest {
 	@Schema(required = true, description = "URL To Shorten")
 	private String longUrl;
 
-	@Schema(description = "Expiration datetime of url")
+	@Schema(description = "Expiration datetime of url, can be supplied as a timestamp. "
+	                      + "Expiration should be a minimum of 5 minutes from the current time",
+			example = "1628973600 represents 2021-08-26T10:26:40Z")
 	private Instant expiry;
 
 	public ShortenUrlRequest(String longUrl) {
