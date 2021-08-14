77c0fef71a84d2a4e6b5a16bfc445589c0cfca66
Anugrah Singhal <anugrahsinghal1@gmail.com>
2021-08-15 01:17:49 +0530

    feat: added JPA method to search on long URL

--- src/main/java/org/infracloud/url/shortener/repository/URLMappingRepository.java
+++ src/main/java/org/infracloud/url/shortener/repository/URLMappingRepository.java
@@ -1,9 +1,13 @@
 package org.infracloud.url.shortener.repository;
 
+import java.util.Optional;
 import org.infracloud.url.shortener.entities.URLMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface URLMappingRepository extends JpaRepository<URLMapping, Long> {
+
+	Optional<URLMapping> findByLongUrl(String url);
+
 }
