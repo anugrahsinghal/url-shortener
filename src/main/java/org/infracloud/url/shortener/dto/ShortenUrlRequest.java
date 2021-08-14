8c76a706d4d9ff002b3c0ecb63c6926b373d2a55
Anugrah Singhal <anugrahsinghal1@gmail.com>
2021-08-15 01:15:53 +0530

    feat: added ability to add expiration to URL's

--- src/main/java/org/infracloud/url/shortener/dto/ShortenUrlRequest.java
+++ src/main/java/org/infracloud/url/shortener/dto/ShortenUrlRequest.java
@@ -2,20 +2,28 @@ package org.infracloud.url.shortener.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.Instant;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.validator.constraints.URL;
 
 @ToString
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(description = "Request Object for Shortening")
 public class ShortenUrlRequest {
 
 	@URL
-    @Schema(required = true, description = "URL To Shorten")
-    private String longUrl;
+	@Schema(required = true, description = "URL To Shorten")
+	private String longUrl;
 
 	@Schema(description = "Expiration datetime of url")
 	private Instant expiry;
+
+	public ShortenUrlRequest(String longUrl) {
+		this.longUrl = longUrl;
+	}
 
 }
